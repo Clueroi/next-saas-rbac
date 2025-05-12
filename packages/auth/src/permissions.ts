4114b84f77560d0ae161d945fcd3089d6ed10d4a
clueroi <eriicsouzaromeroo@gmail.com>
2025-05-12 17:07:20 -0300

    Defined permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -6,14 +6,21 @@ import { Role } from './roles';
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
- ) => void
+) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN: (user, { can, cannot }) => {
+    can('manage', 'all')
+    can(['transfer_ownership', 'update'], 'Organization', { ownerId: { $eq: user.id } })
+    cannot(['transfer_ownership', 'update'], 'Organization')
 
   },
   MEMBER: (user, { can }) => {
+    can('get', 'User')
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
   },
   BILLING: (_, { can }) => {
+    can('manage', 'Billing')
   }
 };
