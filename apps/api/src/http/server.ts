fbb9f2bfc1cebec301715c4b05f04ee8fb925d02
clueroi <eriicsouzaromeroo@gmail.com>
2025-05-14 17:16:51 -0300

    Feat: Authenticate by e-mail and password

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -3,13 +3,15 @@ import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyJwt from "@fastify/jwt";
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
 } from "fastify-type-provider-zod";
-import { createAccount } from "./routes/auth/createAccount";
+import { createAccount } from "./routes/auth/createAccount.controller";
+import { authenticateWithPassword } from "./routes/auth/authenticate-with-password.controller";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -32,13 +34,19 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs'
 }
 )
+
+// Fastify Configuration
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+  
+})
 app.register(fastifyCors)
 
 
 //Routes
 
 app.register(createAccount)
-
+app.register(authenticateWithPassword)
 
 
 
