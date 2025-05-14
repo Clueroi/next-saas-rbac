487146da79075374d3f6ed2ff57f28c9251e64eb
clueroi <eriicsouzaromeroo@gmail.com>
2025-05-14 16:46:42 -0300

    Feat: Setup swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,10 @@
 
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
@@ -11,9 +13,25 @@ import { createAccount } from "./routes/auth/createAccount";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
+//Configurations
+
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
-
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "next-saas",
+      description: "Fullstack saas app with multi-tenant & RBAC",
+      version: "1.0.0"
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+}
+)
 app.register(fastifyCors)
 
 
@@ -25,7 +43,7 @@ app.register(createAccount)
 
 
 app.listen({
-  port:3333
-}).then(()=>{
+  port: 3333
+}).then(() => {
   console.log('Server is running on http://localhost:3333')
 })
